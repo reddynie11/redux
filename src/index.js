e352c907445ac361aa5ab197e3ad98c3a388069e
Narasimha Reddy <macbook@apples-air.intern>
2019-07-17 12:51:48 +0530

    create Store

--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ import App from './App';
 import {Provider} from 'react-redux';
 import {applyMiddleware, createStore} from 'redux';
 
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
 ReactDOM.render(<App />, document.getElementById('root'));
