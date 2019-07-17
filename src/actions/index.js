e616a14bf080d92b14f626e1643defe1e2f7f7d8
Narasimha Reddy <macbook@apples-air.intern>
2019-07-17 14:17:38 +0530

    action creator

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export function movieslist(){
+    return{
+        type : 'MOVIES_LIST',
+        payload :[
+            {id :1, name :'The Spider Man'},
+            {id :2, name :'The Iron Man'},
+            {id :3, name :'The Super Man'},
+            {id :4, name :'The Bat Man'},
+            {id :5, name :'The Ant Man'}
+
+        ]
+    }
+}
