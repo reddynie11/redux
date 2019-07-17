019f1ef969ea6601f0b9f42a8bd653f2660f429d
Narasimha Reddy <macbook@apples-air.intern>
2019-07-17 14:06:13 +0530

    reducer function

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import App from './App';
 
 import {Provider} from 'react-redux';
 import {applyMiddleware, createStore} from 'redux';
+//import reducer
+import reducer from './reducer/index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware()}>  
+    <Provider store={createStoreWithMiddleware(reducer)}>  
         <App />
     </Provider>, document.getElementById('root'));
