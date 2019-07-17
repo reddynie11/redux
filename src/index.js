2aa20f3c146335aa9cf5fcfedb7679b0eafe35f3
Narasimha Reddy <macbook@apples-air.intern>
2019-07-17 12:51:17 +0530

    include middleware

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import {Provider} from 'react-redux';
+import {applyMiddleware, createStore} from 'redux';
 
 ReactDOM.render(<App />, document.getElementById('root'));
